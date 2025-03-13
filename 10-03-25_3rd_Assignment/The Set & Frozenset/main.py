7292dd96ba853a77dbcff584777b23033b02f8a4
Muhammad Bilal Hussain <bh1977955@gmail.com>
2025-03-13 12:28:09 +0500

    update

---
+++ 10-03-25_3rd_Assignment/The Set & Frozenset/main.py
@@ -0,0 +1,41 @@
+# 1st Example (Basic Set Creation) 
+my_set = {1, 2, 3, 4, 2}  
+
+print(my_set)  
+
+# 2nd Example (Add & Remove Elements)
+my_set = {10, 20, 30}
+
+# Element add karna
+my_set.add(40)
+
+# Element remove karna
+my_set.remove(20)
+
+print(my_set)  
+
+# 3rd Example (Set Operations (Union, Intersection))
+A = {1, 2, 3}
+B = {3, 4, 5}
+
+# Union (Combine elements)
+print(A | B) 
+
+# Intersection (Common elements)
+print(A & B)  
+
+# 4th Example (Check Element in Set)
+fruits = {"apple", "banana", "mango"}
+
+if "banana" in fruits:
+    print("Banana mil gaya!")  
+
+# 5th Example (Frozenset (Immutable Set))
+my_frozenset = frozenset([1, 2, 3])
+
+# Frozenset ko print karna
+print(my_frozenset)
+
+# Error aayega (immutable hai)
+my_frozenset.add(4)  
+
