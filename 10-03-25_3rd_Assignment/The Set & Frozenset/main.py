67207e5bb0722622c220ebfc77f8c3a22e1f0163
Muhammad Bilal Hussain <bh1977955@gmail.com>
2025-03-13 12:34:05 +0500

    tsk 3rd update

--- 10-03-25_3rd_Assignment/The Set & Frozenset/main.py
+++ 10-03-25_3rd_Assignment/The Set & Frozenset/main.py
@@ -1,18 +1,18 @@
 # 1st Example (Basic Set Creation) 
-my_set = {1, 2, 3, 4, 2}  
+# my_set = {1, 2, 3, 4, 2}  
 
-print(my_set)  
+# print(my_set) 
 
 # 2nd Example (Add & Remove Elements)
-my_set = {10, 20, 30}
+# my_set = {10, 20, 30}
 
-# Element add karna
-my_set.add(40)
+# # Element add karna
+# my_set.add(40)
 
-# Element remove karna
-my_set.remove(20)
+# # Element remove karna
+# my_set.remove(20)
 
-print(my_set)  
+# print(my_set)
 
 # 3rd Example (Set Operations (Union, Intersection))
 A = {1, 2, 3}
@@ -25,17 +25,17 @@ print(A | B)
 print(A & B)  
 
 # 4th Example (Check Element in Set)
-fruits = {"apple", "banana", "mango"}
+# fruits = {"apple", "banana", "mango"}
 
-if "banana" in fruits:
-    print("Banana mil gaya!")  
+# if "banana" in fruits:
+#     print("Banana mil gaya!")  
 
 # 5th Example (Frozenset (Immutable Set))
-my_frozenset = frozenset([1, 2, 3])
+# my_frozenset = frozenset([1, 2, 3])
 
 # Frozenset ko print karna
-print(my_frozenset)
+# print(my_frozenset)
 
 # Error aayega (immutable hai)
-my_frozenset.add(4)  
+# my_frozenset.add(4)  
 
