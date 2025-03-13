9d6f5b7f6d8c3551eeb1fb50e6b011eb7c472cc5
Muhammad Bilal Hussain <bh1977955@gmail.com>
2025-03-13 05:22:06 +0500

    list tuple

---
+++ 10-03-25 3rd Assignment/Lists, Tuples & Dictionary/main.py
@@ -0,0 +1,13 @@
+# 1st Example (List (Mutable, Ordered))
+fruits = ["apple", "banana", "mango"]
+
+# List ke elements access karna
+print(fruits[0])  # apple
+
+# List me element add karna
+fruits.append("orange")
+print(fruits)  # ['apple', 'banana', 'mango', 'orange']
+
+# List me element update karna
+fruits[1] = "grapes"
+print(fruits)  # ['apple', 'grapes', 'mango', 'orange']
