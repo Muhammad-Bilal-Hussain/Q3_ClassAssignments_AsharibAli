4a5da581392b8308a72f14ddd497e26c729aeecd
Muhammad Bilal Hussain <bh1977955@gmail.com>
2025-03-13 05:31:03 +0500

    update

---
+++ 10-03-25_3rd_Assignment/Lists_Tuples_&_Dictionary/main.py
@@ -0,0 +1,32 @@
+# 1st Example (List (Mutable, Ordered))
+# fruits = ["apple", "banana", "mango"]
+
+# List elements access
+# print(fruits[0])  # apple
+
+# List add
+# fruits.append("orange")
+# print(fruits)  # ['apple', 'banana', 'mango', 'orange']
+
+# List update
+# fruits[1] = "grapes"
+# print(fruits)  # ['apple', 'grapes', 'mango', 'orange']
+
+# 2nd Example (Loop Through List)
+# colors = ["red", "blue", "green"]
+
+# for color in colors:
+#     print(color)
+
+# 3rd Example (Tuple (Immutable, Ordered))
+# coordinates = (10, 20, 30)
+
+# Tuple ke elements access karna
+# print(coordinates[1])  # 20
+
+# Tuple ko loop se print karna
+# for point in coordinates:
+#     print(point)
+
+# 4th ezample (Tuple Unpacking)
+
